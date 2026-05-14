179d074b23bd8827d3fe2826cd5ba214e8a1c123
Ignacio Gonzalez <ig.gonzalezk@duocuc.cl>
2026-05-14 13:27:33 -0400

    avances de microservicio

--- src/main/java/com/tiendafriki/entrega/model/Entrega.java
+++ src/main/java/com/tiendafriki/entrega/model/Entrega.java
@@ -1,5 +1,30 @@
 package com.tiendafriki.entrega.model;
 
+import java.time.LocalDate;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import jakarta.validation.constraints.*;
+import java.util.List;
+
+@Entity
+@Table(name = "entrega")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Entrega {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @NotNull(message = "[+] La fecha no puede estar vacia...")
+    private LocalDate fechaEntrega;
+
+    @Pattern(regexp = "(?i)Entregado|Pendiente|", message = "[+] El estado debe ser Entregado o Pendiente...")
+    @NotBlank(message = "[+] El estado no puede estar vacio...")
+    private String estado;
+    
 }
--- src/main/java/com/tiendafriki/entrega/repository/EntregaRepository.java
+++ src/main/java/com/tiendafriki/entrega/repository/EntregaRepository.java
@@ -1,5 +1,15 @@
 package com.tiendafriki.entrega.repository;
 
-public class EntregaRepository {
+
+import com.tiendafriki.entrega.model.Entrega;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface EntregaRepository extends JpaRepository<Entrega, Integer> {
+    
 
 }
